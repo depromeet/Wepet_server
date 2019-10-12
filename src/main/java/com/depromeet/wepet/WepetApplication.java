6bfcb3309639115f489f8e4447323849a8c58303
iljun <sumusb@naver.com>
2019-10-12 16:50:23 +0900

    init project

---
+++ src/main/java/com/depromeet/wepet/WepetApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.wepet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WepetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WepetApplication.class, args);
+    }
+
+}
