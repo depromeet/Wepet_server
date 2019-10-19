96793295e56acf751989b3051ad397b963987ebb
iljun <sumusb@naver.com>
2019-10-19 17:18:45 +0900

    add runtime environment

--- src/main/java/com/depromeet/wepet/config/JasyptConfig.java
+++ src/main/java/com/depromeet/wepet/config/JasyptConfig.java
@@ -3,11 +3,15 @@ package com.depromeet.wepet.config;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JasyptConfig {
+
+    @Value("${password:#null}")
+    private String password;
 
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
@@ -15,7 +19,7 @@ public class JasyptConfig {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
         config.setAlgorithm("PBEWithMD5AndDES"); //사용할 알고리즘
-        config.setPassword(System.getProperty("password"));
+        config.setPassword(System.getProperty("password") == null ? password : System.getProperty("password"));
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
         config.setProviderName("SunJCE");
