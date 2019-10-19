c591304086a35738a52c4914a45acab097ccd8c4
iljun <sumusb@naver.com>
2019-10-19 17:08:48 +0900

    develop category API

--- src/main/java/com/depromeet/wepet/WepetApplication.java
+++ src/main/java/com/depromeet/wepet/WepetApplication.java
@@ -1,12 +1,13 @@
 package com.depromeet.wepet;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableEncryptableProperties
 public class WepetApplication {
 
     public static void main(String[] args) {
